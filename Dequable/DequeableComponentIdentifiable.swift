f5f7a27cdc560a1aa4aab76505ad315c8f070f44
Rob Nash <robscode@icloud.com>
2018-08-09 16:36:25 +0100

    Remove annotations

--- Dequable/DequeableComponentIdentifiable.swift
+++ Dequable/DequeableComponentIdentifiable.swift
@@ -1,11 +1,3 @@
-//
-//  DequeableComponentIdentifiable.swift
-//  Dequable
-//
-//  Created by Robert Nash on 19/10/2017.
-//  Copyright © 2017 Robert Nash. All rights reserved.
-//
-
 import Foundation
 
 public protocol DequeableComponentIdentifiable: class {
