b074f9f4f31dda3dafab0a824e26e91736c75afe
Rob Nash <robscode@icloud.com>
2018-08-16 16:23:31 +0100

    Handle registration and dequeue of UITableViewHeaderFooterView

--- Dequable/DequeableTableView.swift
+++ Dequable/DequeableTableView.swift
@@ -1,11 +1,17 @@
 import UIKit
 
 public protocol DequeableTableView: Dequeable {
+    func register(headerFooterViewType: DequeableComponentIdentifiable.Type)
     func register(headerFooterViewType: DequeableComponentIdentifiable.Type, hasXib: Bool)
     func dequeue<T>(_ indexPath: IndexPath) -> T where T : UITableViewCell & DequeableComponentIdentifiable
+    func dequeue<T>() -> T where T : UITableViewHeaderFooterView & DequeableComponentIdentifiable
 }
 
 public extension DequeableTableView where Self: UITableView {
+    
+    func register(headerFooterViewType: DequeableComponentIdentifiable.Type) {
+        register(headerFooterViewType: headerFooterViewType, hasXib: false)
+    }
     
     func register(headerFooterViewType: DequeableComponentIdentifiable.Type, hasXib: Bool) {
         let identifier = headerFooterViewType.dequableComponentIdentifier
@@ -21,5 +27,9 @@ public extension DequeableTableView where Self: UITableView {
     
     func dequeue<T>(_ indexPath: IndexPath) -> T where T : UITableViewCell & DequeableComponentIdentifiable {
         return dequeueReusableCell(withIdentifier: T.dequableComponentIdentifier, for: indexPath) as! T
+    }
+    
+    func dequeue<T>() -> T where T : UITableViewHeaderFooterView & DequeableComponentIdentifiable {
+        return dequeueReusableHeaderFooterView(withIdentifier: T.dequableComponentIdentifier) as! T
     }
 }
