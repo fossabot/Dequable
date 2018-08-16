3050654e91582a66f7510949ab5a264b163150b1
Rob Nash <robscode@icloud.com>
2018-08-16 19:38:55 +0100

    Remove pointless UI Test

--- DequableDemo/ViewController.swift
+++ DequableDemo/ViewController.swift
@@ -52,7 +52,7 @@ extension ViewController: UITableViewDataSource {
   }
 
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 10
+    return 20
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
